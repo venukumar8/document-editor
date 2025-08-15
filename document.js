63aabf6e1233fd218e8183a1635ecbbf50d66a52
venukumar22 <venukumarkommu9@gmail.com>
2025-08-15 10:51:21 +0530

    Add files via upload

---
+++ document.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const DocumentSchema = new mongoose.Schema({
+  _id: String,
+  content: Object, // Delta format from Quill
+});
+
+module.exports = mongoose.model("Document", DocumentSchema);
